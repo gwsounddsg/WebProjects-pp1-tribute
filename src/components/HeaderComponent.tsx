414aa9d6c3a208ecf7737e368dbeec7ec5f47485
GW <gwsounddsg@gmail.com>
2020-12-01 19:18:52 -0800

    create HeaderComponent and renders a Hello World to the App

---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default HeaderComponent;
